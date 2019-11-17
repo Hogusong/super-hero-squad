edb0c2fe36a0c93fb41db2215832a38563ef988c
“Young <hogusong1@gmail.com>
2019-11-16 19:31:56 -0500

    create navigation bar.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 
 import './app.css';
+import NavBar from "./components/navbar";
 
 export default class App extends Component {
 
   render() {
     return (
       <div className="App">
-        Open App: Super Heroes
+        <NavBar />
       </div>
     );
   }
---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../css/navbar.css";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <h2 id="left">Assessment</h2>
+      <h2 id="right">Hero</h2>
+    </nav>
+  );
+};
+
+export default NavBar;
